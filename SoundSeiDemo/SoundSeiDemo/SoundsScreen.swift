ec2efe84a4fe1fbcc8b8312144255cc8788a10f5
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-05-19 20:50:52 -0500

    Update SoundsScreen.swift
    included logic from music screen

--- SoundSeiDemo/SoundSeiDemo/SoundsScreen.swift
+++ SoundSeiDemo/SoundSeiDemo/SoundsScreen.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct SoundsScreen: View {
+    let soundItems = [
+        AudioItemModel(title: "Sound 1", audioFileName: "Music/city_birds_waking_rain2.m4a", imageName: "city_birds_waking_rain640x480", category: "urban"),
+        AudioItemModel(title: "Sound 2", audioFileName: "Music/city_birds_waking_rain2.m4a", imageName: "city_birds_waking_rain640x48", category: "urban"),
+    ]
+
     var body: some View {
-        ZStack {
-            Color("CustomColor").ignoresSafeArea(.all)
-            Text("Sounds Screen")
-        }
+        AudioListView(items: soundItems, title: "Soundscapes")
     }
 }
+
 
 #Preview {
     SoundsScreen()
