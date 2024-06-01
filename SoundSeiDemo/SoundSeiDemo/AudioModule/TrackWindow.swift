5b8ea299f3fd8325cc1873a8b4ee085952cf9dd3
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-06-01 18:51:51 -0500

    Update TrackWindow.swift
    Added duration modification functionality

--- SoundSeiDemo/SoundSeiDemo/AudioModule/TrackWindow.swift
+++ SoundSeiDemo/SoundSeiDemo/AudioModule/TrackWindow.swift
@@ -12,28 +12,73 @@ struct TrackWindow: View {
     @ObservedObject var audioController = AudioController()
     var audioFileName: String
     var imageName: String
+    @State private var isShowingPopup = false
+    @State private var selectedPlaybackTime = 60.0 // Default selected playback time
     
     var body: some View {
-            Image(imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-            HStack {
-                Spacer()
-                Button(action: {
-                    if audioController.isPlaying {
-                        audioController.stopSound()
-                    } else {
-                        audioController.playSound(audioFileName: audioFileName)
-                    }
-                }) {
-                    Image(audioController.isPlaying ? "pause_button" : "play_button")
+            ZStack {
+                VStack {
+                    Image(imageName)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(Color("CustomColor"))
+                        .padding()
+                    
+                    HStack {
+                        Button(action: {
+                            //add button action
+                        }) {
+                            Image("add_icon")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(Color("CustomColor"))
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            if audioController.isPlaying {
+                                audioController.stopSound()
+                            } else {
+                                audioController.playSound(audioFileName: audioFileName, playbackTime: selectedPlaybackTime)
+                            }
+                        }) {
+                            Image(audioController.isPlaying ? "pause_button" : "play_button")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(Color("CustomColor"))
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            isShowingPopup.toggle() // Toggle popup visibility
+                        }) {
+                            Image("clock_icon")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(Color("CustomColor"))
+                        }
+                    }
+                    .frame(maxWidth: .infinity) // Ensure HStack does not exceed available width
+                    .padding(.horizontal, 25) // Add some horizontal padding
+                    
                 }
-                Spacer()
+                
+                if isShowingPopup {
+                    Color.black.opacity(0.4)
+                        .edgesIgnoringSafeArea(.all) // Background dimming
+                        .transition(.opacity)
+                    
+                    TimeSlider(audioController: audioController, isShowingPopup: $isShowingPopup, selectedPlaybackTime: $selectedPlaybackTime)
+                        .frame(width: 300, height: 200) // Size of the popup
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .transition(.scale)
+                }
             }
         }
 }
