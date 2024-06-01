6c9ac35b152ad9788bdb8e8ed11b79f099c6fe7b
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-06-01 18:53:03 -0500

    Update AudioController.swift
    Modified to include duration functionality

--- SoundSeiDemo/SoundSeiDemo/AudioModule/AudioController.swift
+++ SoundSeiDemo/SoundSeiDemo/AudioModule/AudioController.swift
@@ -12,15 +12,30 @@ import Combine // framework for processing values over time
 class AudioController: ObservableObject {
     @Published var isPlaying = false // any change to this property will trigger a notification to all UI
     private var audioPlayer: AVAudioPlayer?
-    private var currentPlaybackTime: TimeInterval = 0
+    private var currentPlaybackTime: TimeInterval = 0 // tracks the current playback time
+    private var selectedPlaybackTime: TimeInterval = 0 // stores the selected playback time
     
-    func playSound(audioFileName: String) {
+    var duration: Double {
+            return audioPlayer?.duration ?? 0.0 // duraction of the track
+        }
+    
+    var currentTime: Double {
+            get {
+                return audioPlayer?.currentTime ?? 0.0
+            }
+            set {
+                audioPlayer?.currentTime = newValue
+            }
+        }
+    
+    func playSound(audioFileName: String, playbackTime: Double) {
         let path = Bundle.main.path(forResource: audioFileName, ofType: nil)!
         let url = URL(fileURLWithPath: path)
         
         do {
             if let player = audioPlayer, let playerUrl = player.url, playerUrl == url {
                 // If the same song is being played, resume playback from the stored time
+                audioPlayer?.prepareToPlay()
                 audioPlayer?.currentTime = currentPlaybackTime
                 audioPlayer?.play()
             } else {
@@ -29,6 +44,10 @@ class AudioController: ObservableObject {
                 audioPlayer?.play()
             }
             isPlaying = true
+            selectedPlaybackTime = playbackTime
+            DispatchQueue.main.asyncAfter(deadline: .now() + selectedPlaybackTime) {
+                self.stopSound()
+            }
         } catch {
             print("Couldn't load the file.")
         }
@@ -40,5 +59,9 @@ class AudioController: ObservableObject {
             player.pause()
         }
         isPlaying = false
+    }
+    
+    func seek(to time: Double) {
+            audioPlayer?.currentTime = time
     }
 }
