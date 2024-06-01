aed2c51d89840f12d8042f1c134e3adf59608fd1
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-06-01 18:50:35 -0500

    Create PlaylistView.swift

---
+++ SoundSeiDemo/SoundSeiDemo/PlaylistModule/PlaylistView.swift
@@ -0,0 +1,27 @@
+//
+//  PlaylistView.swift
+//  SoundSeiDemo
+//
+//  Created by Angela Xu on 5/31/24.
+//
+
+import Foundation
+
+// ViewModel to manage playlists
+class PlaylistView: ObservableObject {
+    @Published var playlists: [Playlist] = [] // Array of playlists
+    
+    // Method to add a new playlist
+    func addPlaylist(name: String) {
+        let newPlaylist = Playlist(name: name, durations: [])
+        playlists.append(newPlaylist)
+    }
+    
+    // Method to add duration to a playlist
+    func addDurationToPlaylist(duration: Double, playlistIndex: Int) {
+        guard playlistIndex < playlists.count else {
+            return
+        }
+        playlists[playlistIndex].durations.append(duration)
+    }
+}
