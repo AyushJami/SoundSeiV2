92026840320915d894bef8e29b8692de5f7f3b6a
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-07-06 18:20:59 -0500

    Update AudioItemModel.swift

--- SoundSeiDemo/SoundSeiDemo/AudioModule/AudioItemModel.swift
+++ SoundSeiDemo/SoundSeiDemo/AudioModule/AudioItemModel.swift
@@ -2,14 +2,13 @@
 //  AudioItemModel.swift
 //  SoundSeiDemo
 //
-//  Created by Angela Xu on 5/19/24.
 //
 
 import Foundation
 
-struct AudioItemModel {
-    var title: String
-    var audioFileName: String
-    var imageName: String
-    var category: String
+struct AudioItemModel { // Define a structure to model an audio item
+    var title: String // The title of the audio item
+    var audioFileName: String // The name of the audio file
+    var imageName: String // The name of the image
+    var category: String // The category of the audio item
 }
