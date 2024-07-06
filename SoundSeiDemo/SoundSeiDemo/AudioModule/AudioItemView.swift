862eaf7506fa559dfeb6805ca199a03b89e2083f
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-07-06 18:22:49 -0500

    Update AudioItemView.swift

--- SoundSeiDemo/SoundSeiDemo/AudioModule/AudioItemView.swift
+++ SoundSeiDemo/SoundSeiDemo/AudioModule/AudioItemView.swift
@@ -2,22 +2,22 @@
 //  AudioItemView.swift
 //  SoundSeiDemo
 //
-//  Created by Angela Xu on 5/19/24.
 //
 
 import Foundation
 import SwiftUI
 
-struct AudioItemView: View {
-    var item: AudioItemModel
-    @State private var showTrackWindow = false
+struct AudioItemView: View { // Define a view to display an audio item
+    var item: AudioItemModel // The audio item to be displayed
+    @State private var showTrackWindow = false // State variable to control the display of the track window
     
     var body: some View {
-        HStack {
-            Text(item.title)
+        HStack { // Use an HStack to arrange views horizontally
+            Text(item.title)  // Display the title of the audio item
             Spacer()
+            // Create a navigation link to navigate to TrackWindow view when the icon is tapped
             NavigationLink(destination: TrackWindow(audioFileName: item.audioFileName, imageName: item.imageName)){
-                Image("track_window_icon")
+                Image("track_window_icon") // Display an image icon for the track window (currently no icon)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 30, height: 30)
