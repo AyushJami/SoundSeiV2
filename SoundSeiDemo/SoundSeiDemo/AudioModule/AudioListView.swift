363b0a58222afbde2792d0d111e58907dd38bd43
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-07-06 18:24:14 -0500

    Update AudioListView.swift

--- SoundSeiDemo/SoundSeiDemo/AudioModule/AudioListView.swift
+++ SoundSeiDemo/SoundSeiDemo/AudioModule/AudioListView.swift
@@ -2,23 +2,24 @@
 //  AudioListView.swift
 //  SoundSeiDemo
 //
-//  Created by Angela Xu on 5/19/24.
 //
 
 import Foundation
 import SwiftUI
 
-struct AudioListView: View {
-    let items: [AudioItemModel]
-    let title: String
+struct AudioListView: View { // Define a view to display a list of audio items
+    let items: [AudioItemModel] // Array of audio items to be displayed
+    let title: String // Title of the list
 
     var body: some View {
         VStack {
-            Text(title).font(.title).foregroundStyle(Color("CustomColor"))
+            Text(title).font(.title).foregroundStyle(Color("CustomColor")) // Display the title of the list
+            // Create a navigation view to manage navigation within the list
             NavigationView {
-                List {
+                List { // Create a list to display audio items
+                    // Iterate over each item in the items array
                     ForEach(items, id: \.title) { item in
-                        AudioItemView(item: item)
+                        AudioItemView(item: item) // Display each item using the AudioItemView
                     }
                 }
                 .scrollContentBackground(.hidden)
