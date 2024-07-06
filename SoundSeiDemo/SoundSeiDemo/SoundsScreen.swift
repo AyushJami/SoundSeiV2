8b8a48d33533ca7ff0284b26d6e23f62f9ab8e59
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-07-06 18:14:19 -0500

    Update SoundsScreen.swift

--- SoundSeiDemo/SoundSeiDemo/SoundsScreen.swift
+++ SoundSeiDemo/SoundSeiDemo/SoundsScreen.swift
@@ -2,18 +2,21 @@
 //  SoundsScreen.swift
 //  SoundSeiDemo
 //
-//  Created by Angela Xu on 3/24/24.
 //
 
 import SwiftUI
 
-struct SoundsScreen: View {
-    let soundItems = [
+struct SoundsScreen: View { // Define the sounds screen view of the app
+    let soundItems = [ // Define a list of audio items
+        // Create an audio item model for each sound with its title, audio file name, image name, and category
         AudioItemModel(title: "Sound 1", audioFileName: "Music/city_birds_waking_rain2.m4a", imageName: "city_birds_waking_rain640x480", category: "urban"),
         AudioItemModel(title: "Sound 2", audioFileName: "Music/city_birds_waking_rain2.m4a", imageName: "city_birds_waking_rain640x48", category: "urban"),
+        
+        //add rest of sounds here
     ]
 
     var body: some View {
+        // Use the AudioListView to display the list of sound items
         AudioListView(items: soundItems, title: "Soundscapes")
     }
 }
