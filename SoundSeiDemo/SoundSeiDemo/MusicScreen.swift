8d047153e714d4ae680290a06b64f64853dfa2ad
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-07-06 18:10:12 -0500

    Update MusicScreen.swift

--- SoundSeiDemo/SoundSeiDemo/MusicScreen.swift
+++ SoundSeiDemo/SoundSeiDemo/MusicScreen.swift
@@ -2,21 +2,22 @@
 //  MusicScreen.swift
 //  SoundSeiDemo
 //
-//  Created by Angela Xu on 3/24/24.
 //
 
 import SwiftUI
 import UIKit
 import AVFoundation
 
-struct MusicScreen: View {
-    let musicItems = [
+struct MusicScreen: View { // Define the music screen view of the app
+    let musicItems = [ // Define a list of audio items
+        // Create an audio item model for each song with its title, audio file name, image name, and category
         AudioItemModel(title: "Calm Song 1", audioFileName: "Music/contemplation_time_thought_full725.m4a", imageName: "albireo_blue_green", category: "Calm"),
         AudioItemModel(title: "Calm Song 2", audioFileName: "calm_song_1.m4a", imageName: "albireo_blue_green", category: "Calm"),
         // Add other songs here
     ]
 
     var body: some View {
+        // Use the AudioListView to display the list of music items on the Music screen
         AudioListView(items: musicItems, title: "Music")
     }
 }
