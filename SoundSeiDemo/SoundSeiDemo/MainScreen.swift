9afb0084e1b836ee2c5d43c4915d43a588e8e939
Angelax234 <38166852+Angelax234@users.noreply.github.com>
2024-07-06 18:05:42 -0500

    Update MainScreen.swift

--- SoundSeiDemo/SoundSeiDemo/MainScreen.swift
+++ SoundSeiDemo/SoundSeiDemo/MainScreen.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct MainScreen: View {
     var body: some View {
-        ZStack {
+        ZStack { // Use a ZStack to layer the background color and the tab view
             Color("CustomColor").ignoresSafeArea(.all)
-            TabView {
-                MusicScreen().tabItem {
+            TabView { // Create a tab view for navigation between different screens
+                MusicScreen().tabItem { // First tab: MusicScreen - list of all the music options (ability to play/pause, add to playlist, change duration, and see different vizualizers)
                     Label("Music", image: "MusicTab")
                 }
-                SoundsScreen().tabItem {
+                SoundsScreen().tabItem { // Second tab: SoundsScreen - list of all the sound options (same functionality as music screen)
                     Label("Sounds", image: "SoundsTab")
                 }
-                PlaylistsScreen().tabItem {
+                PlaylistsScreen().tabItem { // Third tab: PlaylistsScreen - list of playlists created from Music & Sounds that the user puts together
                     Label("Playlists", image: "PlaylistsTab")
                 }
             }.accentColor(Color.white)
